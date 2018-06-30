e4da6410b2d7a3cca5dc7d01ff6dc3d9a72583e8
Adam Klingenberg <adam.klingenberg@ihs.com>
2018-06-30 21:11:31 +0200

    cleanup

--- POM.Tests/TestBase.cs
+++ POM.Tests/TestBase.cs
@@ -1,76 +1,38 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using POM.Tools;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace POM.Tests
 {
-    //[TestFixture]
     public class TestBase
     {
-
         public IWebDriver Driver
         {
             get; private set;
         }
 
-
         [SetUp]
         public void DriverOpen()
         {
             Driver = DriverFactory.GetDriver();
-
         }
 
         [TearDown]
         public void DriverClose()
         {
-            //Output.WriteLine("Starting [TearDown] for [" + TestContext.CurrentContext.Test.FullName + "]");
             if (TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Failure) || TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Error))
             {
-
-
-                var msg = Environment.NewLine + TestContext.CurrentContext.Result.Message + Environment.NewLine + TestContext.CurrentContext.Result.StackTrace;
-                var fullPath = Path.GetFullPath(SaveScreenshot());
-
+                var msg = TestContext.CurrentContext.Result.Message + Environment.NewLine + TestContext.CurrentContext.Result.StackTrace;
+                var fullPath = Path.GetFullPath(SaveScreenshot());                
                 TestContext.AddTestAttachment(fullPath, msg);
-
-                //if (TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Error))
-                //    Log.Warn("Unexpected Exception:" + msg);
-                //else
-                //    Log.Warn("Assertion Failed:" + msg);
-
-
-                //string xmlData = System.IO.Path.Combine(TestContext.CurrentContext.WorkDirectory, "xmlData.xml");
-
-
-                //SavePageSource();
-
             }
 
             Driver?.Quit();
             Driver = null;
 
-            //var browserLog = Browser.GetLog().ToList();
-            //if (browserLog.Count > 0)
-            //{
-            //    Log.Warn("There were [" + browserLog.Count + "] message(s) on browser console:\n " + string.Join("\n ", browserLog));
-            //}
-
-            //if (KeepSession && !TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Error))
-            //    Browser.Close();
-            //else
-            //    Browser.Quit();
-            //Log.InfoFormat("-> Ended:" + TestContext.CurrentContext.Test.FullName);
-            //Output.WriteLine("Finished [TearDown] for [" + TestContext.CurrentContext.Test.FullName + "]");
         }
         private string GetSafeFilename(string filename)
         {
@@ -79,7 +41,6 @@ namespace POM.Tests
             return safePath;
         }
 
-        string _screenshotsFolderPath = "";
         public void SaveScreenshot(string screenshotsFolderPath)
         {
             try
@@ -89,7 +50,7 @@ namespace POM.Tests
             }
             catch (Exception e)
             {
-                throw new Exception($"Problem while saving screenshot:[{screenshotsFolderPath}]\nEXCEPTION:{e.Message}", e);
+//                throw new Exception($"Problem while saving screenshot:[{screenshotsFolderPath}]\nEXCEPTION:{e.Message}", e);
             }
         }
         public virtual string SaveScreenshot()
@@ -97,39 +58,6 @@ namespace POM.Tests
             var fileName = $"{GetSafeFilename(TestContext.CurrentContext.Test.FullName)}.{DateTime.Now:yyyyMMddHHmmss}.png";
             SaveScreenshot(fileName);
             return fileName;
-
-            //var filePath = $"{_screenshotsFolderPath}{fileName}";
-            //try
-            //{
-            //    Browser.SaveScreenshot(filePath);
-            //}
-            //catch { }
-            //if (File.Exists(filePath))
-            //{
-            //    //Console.WriteLine("##teamcity[publishArtifacts '" + filePath + "']");
-            //    Log.InfoFormat("Screenshot taken: \n\"{0}\"", new Uri(filePath));
-            //}
-            //else
-            //    Log.InfoFormat("Screenshot was not generated.");
-            //return filePath;
-        }
-
-        protected virtual string SavePageSource()
-        {
-            var fileName = $"{GetSafeFilename(TestContext.CurrentContext.Test.FullName)}.{DateTime.Now:yyyyMMddHHmmss}.txt";
-            var filePath = $"{_screenshotsFolderPath}{fileName}";
-            //try
-            //{
-            //    var pageSource = Browser.PageSource;
-            //    if (!string.IsNullOrEmpty(pageSource))
-            //    {
-            //        File.WriteAllText(filePath, pageSource);
-            //        //Console.WriteLine("##teamcity[publishArtifacts '" + filePath + "']");
-            //        Log.InfoFormat("PageSource saved: \n\"{0}\"", new Uri(filePath));
-            //    }
-            //}
-            //catch { }
-            return filePath;
         }
     }
 }
