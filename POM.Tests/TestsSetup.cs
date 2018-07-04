897cc49b5a89f703b509f953b89f266ca63d566b
Adam Klingenberg <adam.klingenberg@ihs.com>
2018-07-04 03:05:11 +0200

    refact

--- POM.Tests/TestsSetup.cs
+++ POM.Tests/TestsSetup.cs
@@ -1,11 +1,6 @@
 ﻿using NUnit.Framework;
 using POM.Tests.Tools;
 using POM.Tools;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace POM.Tests
 {
