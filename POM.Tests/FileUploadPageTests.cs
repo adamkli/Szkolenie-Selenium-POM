c8c039f200ba45a5e4204b738e19cd6fa50345df
alpha <alpha@sedaco.pl>
2018-07-04 16:31:36 +0200

    new tests

---
+++ POM.Tests/FileUploadPageTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POM.Tests
+{
+    [TestFixture]
+    class FileUploadPageTests : TestWith<FileUploadPageObject>
+    {
+        [Test]
+        public void FileUploadShouldSucceed()
+        {
+            string path = Path.GetFullPath(@"TestData/UsersPasswords.csv");
+            Page.UploadFile(path);
+            Assert.IsTrue(Page.FileUploaded.Contains("UsersPasswords.csv"));
+        }
+    }
+}
---
+++ POM/FileUploadPageObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoIt;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace POM
+{
+    public class FileUploadPageObject : PageBase
+    {
+        [FindsBy(How = How.Id, Using = "file-upload")]
+        private IWebElement selectFileElement;
+        [FindsBy(How = How.Id, Using = "uploaded-files")]
+        private IWebElement uploadedFilesElement;
+        [FindsBy(How = How.Id, Using = "file-submit")]
+        private IWebElement fileSubmitElement;
+
+        public string FileUploaded => uploadedFilesElement.Text;
+            
+        protected override string GetRelativeURL() => "upload";
+        public FileUploadPageObject(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void UploadFile(string fileName)
+        {
+            selectFileElement.Click();
+            AutoItX.Send(fileName);
+            AutoItX.Send("{ENTER}");
+
+            //selectFileElement.SendKeys(fileName);
+            
+            fileSubmitElement.Click();
+        }
+    }
+}
